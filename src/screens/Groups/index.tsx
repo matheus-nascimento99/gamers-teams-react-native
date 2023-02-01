be2a5dd26ccf7318abd6d93e11f5729e1e604224
Matheus Nascimento <mnsergio59@gmail.com>
2023-02-01 18:17:25 -0300

    efsffd

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,15 @@
+import { useState } from 'react';
+import { FlatList, Text } from 'react-native';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
 import * as S from './style'
 
 export const Groups = () => {
+
+  const [groups, setGroups] = useState<string[]>([]);
+
   return (
     <S.Container>
       <Header />
@@ -11,8 +17,21 @@ export const Groups = () => {
         title='Turmas'
         subtitle='jogue com a sua turma'
       />
-      <GroupCard 
-        title='Galera do ignite'
+      
+      <FlatList
+        data={groups}
+        renderItem={({item, index}) => (
+          <GroupCard 
+            title={item} 
+          /> 
+        )}
+        keyExtractor={(item) => item}
+        contentContainerStyle={groups.length === 0 && {flex:1}}
+        ListEmptyComponent={()=>(
+          <ListEmpty 
+            message='Que tal cadastrar a primeira turma?' 
+          />
+        )}
       />
     </S.Container>
   );
